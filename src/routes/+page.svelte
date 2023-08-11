c2261e7c4df3b28a3c467b89c54f535742b690d0
--null <--null>
2023-08-11 18:03:51 +0200

    feat(page): add hero on root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,26 +13,44 @@
 </svelte:head>
 
 <TopAppBar variant="static" {dense}>
-	<Row>
-		<Section>
-			<IconButton class="material-icons">menu</IconButton>
-			<Title>Meta Names</Title>
-		</Section>
+	<div class="topbar-container">
+		<Row>
+			<Section>
+				<IconButton class="material-icons">menu</IconButton>
+				<Title>Meta Names</Title>
+			</Section>
 
-		<Section align="end" toolbar>
-			<IconButton class="material-icons" aria-label="Wallet">wallet</IconButton>
-		</Section>
-	</Row>
+			<Section align="end" toolbar>
+				<IconButton class="material-icons" aria-label="Wallet">wallet</IconButton>
+			</Section>
+		</Row>
+	</div>
 </TopAppBar>
 
-<div class="content">
-	<h1>Meta Names</h1>
+<div class="hero hero-primary">
+	<div class="content">
+		<h1>Meta Names</h1>
+		<h5>The only name you need</h5>
+	</div>
 </div>
 
 <style>
+	.topbar-container {
+		padding: 0.5rem 0;
+	}
 	.content {
-		max-width: 960px;
+		max-width: 1280px;
 		margin: 0 auto;
-		padding: 1em;
+		padding: 8rem;
+		text-align: center;
+
+		@media only screen and (max-width: 600px) {
+			padding: 10%;
+		}
+	}
+
+	.hero,
+	.hero-primary {
+		background-color: var(--mdc-theme-primary);
 	}
 </style>
