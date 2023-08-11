96137ee2255bbdf4d71a77a5a674d6b96591264e
--null <--null>
2023-08-11 15:26:42 +0200

    feat(page): update root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Row, Title } from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+
+	import Section from '@smui/top-app-bar/src/Section.svelte';
+	import TopAppBar from '@smui/top-app-bar/src/TopAppBar.svelte';
+
+	let dense = true;
+</script>
+
+<svelte:head>
+	<title>Meta Names</title>
+</svelte:head>
+
+<TopAppBar variant="static" {dense}>
+	<Row>
+		<Section>
+			<IconButton class="material-icons">menu</IconButton>
+			<Title>Meta Names</Title>
+		</Section>
+
+		<Section align="end" toolbar>
+			<IconButton class="material-icons" aria-label="Wallet">wallet</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+
+<div class="content">
+	<h1>Meta Names</h1>
+</div>
+
+<style>
+	.content {
+		max-width: 960px;
+		margin: 0 auto;
+		padding: 1em;
+	}
+</style>
