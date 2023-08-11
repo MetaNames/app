4e53e8dc3fc1bcd984b6853df808f035d1741b8e
--null <--null>
2023-08-11 22:07:34 +0200

    chore(deps): add meta names sdk

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { MetaNames, Enviroment } from 'meta-names-sdk'
+
+export const metaNames = new MetaNames(Enviroment.testnet)
