d93abd42df5bad3b906172aab6cd90efc29370b2
--null <--null>
2023-08-12 15:32:12 +0200

    docs: remove unneeded comment

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
 import { MetaNames, Enviroment } from 'meta-names-sdk'
 
 export const metaNames = new MetaNames(Enviroment.testnet)
