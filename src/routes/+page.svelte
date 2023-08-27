1e167da481148efb484e3bf39ac792fb1ce6dd81
--null <--null>
2023-08-27 14:24:58 +0200

    refactor: move layout to special file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Layout from './Layout.svelte';
 	import LookUp from './LookUp.svelte';
 
 	import '../styles/hero.scss';
@@ -9,23 +8,21 @@
 	<title>Meta Names</title>
 </svelte:head>
 
-<Layout>
-	<div class="hero-primary">
-		<div class="content">
-			<h1>Meta Names</h1>
-			<h5>The only name you need</h5>
+<div class="hero-primary">
+	<div class="content">
+		<h1>Meta Names</h1>
+		<h5>The only name you need</h5>
+	</div>
+</div>
+<div class="hero-secondary">
+	<div class="content">
+		<h1>Look Up</h1>
+		<h5>Find your Meta Name</h5>
+		<div class="lookup-container">
+			<LookUp />
 		</div>
 	</div>
-	<div class="hero-secondary">
-		<div class="content">
-			<h1>Look Up</h1>
-			<h5>Find your Meta Name</h5>
-			<div class="lookup-container">
-				<LookUp />
-			</div>
-		</div>
-	</div>
-</Layout>
+</div>
 
 <style lang="scss">
 	.lookup-container {
