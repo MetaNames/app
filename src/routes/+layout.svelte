a219b9f482aff03d9e36f46bf7cebc83f30af314
--null <--null>
2023-08-27 15:38:44 +0200

    feat: reroganize home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,48 +1,58 @@
 <script lang="ts">
 	import IconButton from '@smui/icon-button';
 	import Section from '@smui/top-app-bar/src/Section.svelte';
-	import TopAppBar, { AutoAdjust, Row, Title } from '@smui/top-app-bar';
+	import TopAppBar, { Row, Title } from '@smui/top-app-bar';
 	import { Anchor } from '@smui/menu-surface';
 
 	import WalletConnect from './WalletConnect.svelte';
 
 	let anchor: HTMLDivElement;
 	let anchorClasses: { [k: string]: boolean } = {};
-	let topAppBar: TopAppBar;
-	let walletConnect: WalletConnect;
 </script>
 
-<TopAppBar bind:this={topAppBar} variant="standard">
-	<div
-		class={Object.keys(anchorClasses).join(' ')}
-		use:Anchor={{
-			addClass: (className) => {
-				if (!anchorClasses[className]) {
-					anchorClasses[className] = true;
+<main>
+	<TopAppBar variant="static">
+		<div
+			class={Object.keys(anchorClasses).join(' ')}
+			use:Anchor={{
+				addClass: (className) => {
+					if (!anchorClasses[className]) {
+						anchorClasses[className] = true;
+					}
+				},
+				removeClass: (className) => {
+					if (anchorClasses[className]) {
+						delete anchorClasses[className];
+						anchorClasses = anchorClasses;
+					}
 				}
-			},
-			removeClass: (className) => {
-				if (anchorClasses[className]) {
-					delete anchorClasses[className];
-					anchorClasses = anchorClasses;
-				}
-			}
-		}}
-		bind:this={anchor}
-	>
-		<Row>
-			<Section>
-				<IconButton class="material-icons">menu</IconButton>
-				<Title>Meta Names</Title>
-			</Section>
+			}}
+			bind:this={anchor}
+		>
+			<Row>
+				<Section>
+					<IconButton class="material-icons">menu</IconButton>
+					<Title>Meta Names</Title>
+				</Section>
 
-			<Section align="end" toolbar>
-				<WalletConnect {anchor} bind:this={walletConnect} />
-			</Section>
-		</Row>
-	</div>
-</TopAppBar>
-
-<AutoAdjust {topAppBar} on:mousewheel={() => walletConnect.closeMenu()}>
+				<Section align="end" toolbar>
+					<WalletConnect {anchor} />
+				</Section>
+			</Row>
+		</div>
+	</TopAppBar>
 	<slot />
-</AutoAdjust>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
+
+		gap: 1rem;
+
+		min-height: 100vh;
+		background-color: var(--mdc-theme-background);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,36 @@
 <script lang="ts">
 	import LookUp from './LookUp.svelte';
-
-	import '../styles/hero.scss';
 </script>
 
 <svelte:head>
-	<title>Meta Names</title>
+	<title>App | Meta Names</title>
 </svelte:head>
 
-<div class="hero-secondary">
-	<div class="content">
-		<h3>Find your Meta Name</h3>
-		<p>Choose your decentralised name to use it for wallets, social, websites, and more!</p>
-		<div class="lookup-container">
-			<LookUp />
-		</div>
+<div class="content">
+	<h3>Find your Meta Name</h3>
+	<p>Choose your decentralised name to use it for wallets, social, websites, and more!</p>
+	<div class="lookup-container">
+		<LookUp />
 	</div>
 </div>
 
 <style lang="scss">
+	h3 {
+		margin-top: 0;
+		margin-bottom: 0.5rem;
+	}
+
 	.lookup-container {
 		display: inline-block;
+	}
+
+	.content {
+		align-self: center;
+		flex-grow: 1;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
