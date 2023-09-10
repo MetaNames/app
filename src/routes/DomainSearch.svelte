fdfca97717e1f520df66e87406ee32d7866b9864
--null <--null>
2023-09-10 10:46:35 +0200

    feat(search): add icon into form

--- src/routes/DomainSearch.svelte
+++ src/routes/DomainSearch.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { metaNamesSdk } from '$lib';
 
-	import Button from '@smui/button';
 	import Card, { Content as CardContent } from '@smui/card';
 	import Textfield from '@smui/textfield';
 	import HelperText from '@smui/textfield/helper-text';
 	import type { Domain as DomainModel } from '@metanames/sdk/lib/models/domain';
+	import IconButton from '@smui/icon-button/src/IconButton.svelte';
 
 	const validator = metaNamesSdk.domainRepository.domainValidator;
 
@@ -18,7 +18,7 @@
 	$: invalid = domainName !== '' && !validator.validate(domainName, { raiseError: false });
 
 	async function submit() {
-		if (invalid) return
+		if (invalid) return;
 
 		nameSearched = domainName;
 		isLoading = true;
@@ -33,17 +33,24 @@
 
 <form class="lookup-form" on:submit|preventDefault={submit}>
 	<formgroup>
-		<Textfield variant="outlined" bind:value={domainName} bind:invalid label="Domain">
+		<Textfield
+			variant="outlined"
+			bind:value={domainName}
+			bind:invalid
+			label="Domain"
+			withTrailingIcon={true}
+		>
+			<svelte:fragment slot="trailingIcon">
+				<div class="submit">
+					<IconButton class="material-icons">search</IconButton>
+				</div>
+			</svelte:fragment>
 			<svelte:fragment slot="helper">
 				{#if errors.length > 0}
 					<HelperText slot="helper">{errors.join(', ')}</HelperText>
 				{/if}
 			</svelte:fragment>
 		</Textfield>
-	</formgroup>
-
-	<formgroup class="submit">
-		<Button variant="raised" type="submit">Find</Button>
 	</formgroup>
 </form>
 {#if domain}
@@ -119,5 +126,9 @@
 	.domain-link {
 		text-decoration: none;
 		color: inherit;
+	}
+
+	.submit {
+		align-self: center;
 	}
 </style>
