a2e8fdeb3ea95005a3190193e4199f1d324d8f1d
Marco <marco@yeboster.com>
2023-09-12 21:19:33 +0200

    feat(domain): add avatar

--- src/routes/Domain.svelte
+++ src/routes/Domain.svelte
@@ -13,6 +13,10 @@
 
 	$: domainAvatar = domain.name && toSvg(domain.name, 250);
 
+	import { toSvg } from "jdenticon";
+
+	$: domainAvatar = domain.name && toSvg(domain.name, 600);
+
 	export let domain: Domain;
 
 	let selectedRecord: string;
