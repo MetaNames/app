d39d4b8bdb754a5d6f0dc2d4b8cde1d7bce7f59c
Yeboster <marco@yeboster.com>
2023-11-27 10:38:38 +0100

    Improve footer (#20)
    * feat: permit logo customization
    
    * feat: improve footer
    
    * feat: align content to center

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,18 +2,21 @@
 	import Logo from './Logo.svelte';
 </script>
 
-<footer>
+<footer class="card-primary">
 	<div class="logo">
-		<Logo />
+		<Logo color="light" />
 		<span>Meta Names</span>
 	</div>
 	<div class="links">
 		<a href="mailto:metanames@proton.me" target="_blank">Contact us</a>
+		<a href="https://discord.gg/bn6tJNRgUS" target="_blank">Discord</a>
 		<a href="https://x.com/metanames_" target="_blank">Twitter</a>
+		<a href="https://github.com/metanames" target="_blank">GitHub</a>
+		<a href="https://github.com/metanames/sdk/wiki" target="_blank">API Docs</a>
 	</div>
 </footer>
 
-<style>
+<style lang="scss">
 	.logo {
 		display: flex;
 		flex-direction: row;
@@ -22,13 +25,20 @@
 
 	.links {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		align-items: center;
+		justify-content: space-evenly;
+
+		a {
+			margin: 0 0.5rem;
+		}
 	}
 
 	a,
 	span {
-		color: var(--mdc-theme-on-primary);
+		font-size: small;
+		color: var(--mdc-theme-on-background);
+		text-decoration: none;
 	}
 
 	footer {
@@ -36,9 +46,7 @@
 		flex-direction: row;
 		justify-content: space-evenly;
 		align-items: center;
-		padding: 1rem;
-
-		background-color: var(--mdc-theme-primary);
+		padding: 1rem 0;
 
 		@media only screen and (max-width: 600px) {
 			flex-direction: column;
