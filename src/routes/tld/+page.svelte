65525c87cd23bf9358fd0b811309cf52884fac67
Marco <marco@yeboster.com>
2024-01-02 15:26:23 +0100

    feat: add tld page

---
+++ src/routes/tld/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Domain from '../../components/Domain.svelte';
+
+	import { metaNamesSdk } from "$lib/stores";
+	import { Domain as DomainModel, type IDomain } from "@metanames/sdk";
+
+  const contractAddress = $metaNamesSdk.config.contractAddress;
+
+  const domainData: IDomain = {
+    name: "meta",
+    tld: '',
+    owner: contractAddress,
+    tokenId: NaN,
+    records: new Map(),
+  }
+
+  const domain = new DomainModel(domainData, $metaNamesSdk.contract);
+</script>
+
+<svelte:head>
+	<title>Meta | Meta Names</title>
+</svelte:head>
+
+<div class="content domain-container">
+  <Domain {domain} />
+</div>
