965315ee1a84a88ef562b69f6b436931b421e9b3
Marco <marco@yeboster.com>
2024-01-02 17:01:56 +0100

    feat: add browser url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,7 @@
 import type { PermissionTypes } from 'partisia-sdk/dist/sdk-listeners';
 
 type Config = {
+	browserUrl: string;
 	chainId: string;
 	dAppName: string;
 	environment: 'test' | 'prod';
@@ -8,9 +9,11 @@ type Config = {
 };
 
 const environment = import.meta.env.VITE_ENV ?? 'test';
+const browserUrl = `https://browser${environment === 'test' ? '.testnet' : ''}.partisiablockchain.com`;
 const chainId = `Partisia Blockchain${environment === 'test' ? ' Testnet' : ''}`;
 
 const config: Config = {
+	browserUrl,
 	chainId,
 	environment,
 	dAppName: 'Meta Names',
