96a0a677570aab4fd426335cae72f1c1d417d478
Marco <marco@yeboster.com>
2024-01-18 17:59:50 +0100

    feat(register): replace history state during redirect to existing domain

--- src/routes/register/[name]/+page.svelte
+++ src/routes/register/[name]/+page.svelte
@@ -26,13 +26,13 @@
 	onMount(async () => {
 		domain = await $metaNamesSdk.domainRepository.find(domainName);
 
-		if (domain) goto(`/domain/${domain.name}`);
+		if (domain) goto(`/domain/${domain.name}`, { replaceState: true });
 
 		if (parentDomainName && parentDomainName !== tld) {
 			parentDomain = await $metaNamesSdk.domainRepository.find(parentDomainName);
 			if (!parentDomain) {
 				alertMessage.set('Parent domain not found, please register it first.');
-				goto(`/register/${parentDomainName}`);
+				goto(`/register/${parentDomainName}`, { replaceState: true });
 			}
 		} else parentDomain = null;
 	});
