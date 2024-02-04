e9a71652183f6989944a9e8fbd70633f7276d8a9
Marco <marco@yeboster.com>
2024-02-04 11:30:57 +0100

    refactor(store): select coin on init

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,6 @@
 import { metaNamesSdkFactory } from '$lib';
 import type { BYOCSymbol } from '@metanames/sdk';
-import { derived, writable } from 'svelte/store';
+import { derived, get, writable } from 'svelte/store';
 
 export const metaNamesSdk = writable(metaNamesSdkFactory());
 export const walletAddress = writable<string | undefined>();
@@ -11,4 +11,4 @@ export const walletConnected = derived(
 export const alertMessage = writable<string | undefined>();
 export const alertTransaction = writable<string | undefined>();
 
-export const selectedCoin = writable<BYOCSymbol>();
+export const selectedCoin = writable<BYOCSymbol>(get(metaNamesSdk).config.byoc[0].symbol);
