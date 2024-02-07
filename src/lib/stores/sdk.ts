db7d83d052cc6fece1c341a10fbd0a1891069f3a
Marco <marco@yeboster.com>
2024-02-07 17:55:54 +0100

    refactor: default coin is ETH

--- src/lib/stores/sdk.ts
+++ src/lib/stores/sdk.ts
@@ -1,6 +1,9 @@
-import { metaNamesSdkFactory } from '$lib';
-import type { BYOCSymbol } from '@metanames/sdk';
+import { config, metaNamesSdkFactory } from '$lib';
+import type { BYOC, BYOCSymbol } from '@metanames/sdk';
 import { get, writable } from 'svelte/store';
 
 export const metaNamesSdk = writable(metaNamesSdkFactory());
-export const selectedCoin = writable<BYOCSymbol>(get(metaNamesSdk).config.byoc[0].symbol);
+
+const byocs = get(metaNamesSdk).config.byoc
+const initialByoc = config.environment === 'test' ? byocs[0].symbol : (byocs.find(byoc => byoc.symbol === 'ETH') as BYOC).symbol;
+export const selectedCoin = writable<BYOCSymbol>(initialByoc);
