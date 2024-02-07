24bb393e38fcda7d139e72a6f4e486d7cc7a2772
Marco <marco@yeboster.com>
2024-02-07 15:21:31 +0100

    refactor: stats

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,18 @@
 import { cache, metaNamesSdk } from "$lib/server";
 
+type Stats = {
+  recentDomains: { name: string; createdAt: Date }[];
+};
+
 export async function load() {
-  let stats = cache.get("stats");
+  let stats = cache.get("stats") as Stats | undefined;
   if (stats) return stats;
 
   const recentDomains = await metaNamesSdk.domainRepository.getAll()
     .then((domains) => domains.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()).slice(0, 8)
       .map((domain) => ({ name: domain.name, createdAt: domain.createdAt })));
 
-  stats = {
-    stats: {
-      recentDomains
-    }
-  };
+  stats = { recentDomains };
   cache.put("stats", stats);
 
   return stats;
