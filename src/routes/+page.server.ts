69656b73d2dacdaf4f2c9cfbed450122522cf1d4
Marco <marco@yeboster.com>
2024-02-07 14:58:44 +0100

    feat: cache stats

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,19 @@
-import { metaNamesSdk } from "$lib/server";
+import { cache, metaNamesSdk } from "$lib/server";
 
 export async function load() {
+  let stats = cache.get("stats");
+  if (stats) return stats;
+
   const recentDomains = await metaNamesSdk.domainRepository.getAll()
     .then((domains) => domains.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()).slice(0, 8)
       .map((domain) => ({ name: domain.name, createdAt: domain.createdAt })));
 
-  return {
+  stats = {
     stats: {
       recentDomains
     }
   };
+  cache.put("stats", stats);
+
+  return stats;
 }
