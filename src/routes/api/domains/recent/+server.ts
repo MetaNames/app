710d312502fbdaa85eb513ce686ecb5b96412be1
Marco <marco@yeboster.com>
2024-02-08 21:39:20 +0100

    refactor: move recent domain to api call

---
+++ src/routes/api/domains/recent/+server.ts
@@ -0,0 +1,10 @@
+import { metaNamesSdk } from "$lib/server";
+import { json } from '@sveltejs/kit';
+
+export async function GET() {
+	const recentDomains = await metaNamesSdk.domainRepository.getAll()
+		.then((domains) => domains.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()).slice(0, 8)
+			.map((domain) => ({ name: domain.name, createdAt: domain.createdAt })));
+
+	return json(recentDomains, { headers: { 'Cache-Control': 'max-age=500, public' } });
+}
