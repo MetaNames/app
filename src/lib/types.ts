ce841afbd7c666253c4f4d274025c75dff8f3f32
Marco <marco@yeboster.com>
2024-02-10 23:53:43 +0100

    feat: enhance snackbar to support action

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,11 @@ export enum DomainTab {
   details = 'details',
   settings = 'settings'
 }
+
+export interface AlertMessage {
+  message: string;
+  action?: {
+    label: string;
+    callback: () => void;
+  }
+}
