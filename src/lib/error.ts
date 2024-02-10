d5f0d4daf80706745954173613922efdb57461da
Marco <marco@yeboster.com>
2024-02-10 23:54:28 +0100

    feat: add deposit action in case of insufficient balance on snackbar

---
+++ src/lib/error.ts
@@ -0,0 +1,8 @@
+import type { BYOCSymbol } from "@metanames/sdk";
+
+export class InsufficientBalanceError extends Error {
+  constructor(public coin: BYOCSymbol) {
+    super(`Insufficient balance for ${coin}`);
+    this.name = 'InsufficientBalanceError';
+  }
+}
--- src/lib/url.ts
+++ src/lib/url.ts
@@ -2,3 +2,5 @@ import { config } from "./config";
 
 export const explorerTransactionUrl = (transactionId: string) =>
   `${config.browserUrl}/transactions/${transactionId}`;
+
+export const bridgeUrl = `${config.browserUrl}/bridge`;
