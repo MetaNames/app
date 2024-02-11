0b4ee2a28c30b9686d63b2a83fa7bfed0ced3952
Marco <marco@yeboster.com>
2024-02-11 13:10:26 +0100

    fix: date parsing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,9 @@
 import type { ITransactionIntent } from "@metanames/sdk";
 import { alertTransaction } from "./stores/main";
 
-export const formatDate = (date: Date) => {
+export const formatDate = (date: string | Date) => {
+	if (typeof date === 'string') date = new Date(date);
+
 	const day = date.getDate();
 	const month = date.toLocaleString('default', { month: 'long' });
 	const year = date.getFullYear();
