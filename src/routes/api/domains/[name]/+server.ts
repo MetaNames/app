0eb9f4e4e8f0cde81812251d598b2d1d586fd81a
Marco <marco@yeboster.com>
2024-02-14 13:22:18 +0100

    feat: disable cache on server

--- src/routes/api/domains/[name]/+server.ts
+++ src/routes/api/domains/[name]/+server.ts
@@ -2,7 +2,7 @@ import { metaNamesSdk } from "$lib/server";
 import { json } from "@sveltejs/kit";
 
 export async function GET({ params: { name } }) {
-  const domain = await metaNamesSdk.domainRepository.find(name)
+  const domain = await metaNamesSdk.domainRepository.find(name, { cache: false })
 
   return json({ domain: domain?.toJSON() })
 }
--- src/routes/api/domains/[name]/check/+server.ts
+++ src/routes/api/domains/[name]/check/+server.ts
@@ -3,13 +3,13 @@ import { json } from "@sveltejs/kit";
 
 export async function GET({ params: { name } }) {
   const analyzedDomain = metaNamesSdk.domainRepository.analyze(name);
-  const domain = await metaNamesSdk.domainRepository.find(name);
+  const domain = await metaNamesSdk.domainRepository.find(name, { cache: false });
   const parentDomainName = analyzedDomain.parentId;
   const tld = analyzedDomain.tld
 
   let parentDomain = null;
   if (!domain && parentDomainName && parentDomainName !== tld)
-    parentDomain = await metaNamesSdk.domainRepository.find(parentDomainName);
+    parentDomain = await metaNamesSdk.domainRepository.find(parentDomainName, { cache: false });
 
   const data = {
     domainPresent: !!domain,
