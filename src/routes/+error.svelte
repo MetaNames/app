87dd703cce3c53125817c58e96de8ab28712abda
Marco <marco@yeboster.com>
2024-02-16 16:28:23 +0100

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { page } from '$app/stores';
+	import GoBackButton from 'src/components/GoBackButton.svelte';
+</script>
+
+<svelte:head>
+	<title>{$page.status}</title>
+</svelte:head>
+
+<div class="content">
+	{#if $page.status === 404}
+		<h2>Not found!</h2>
+		<GoBackButton />
+	{:else}
+		<h2>Something went wrong</h2>
+		<GoBackButton />
+	{/if}
+</div>
