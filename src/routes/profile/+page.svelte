6c89fe275e4fd0826c9600200effea6ca54c94e0
Marco <marco@yeboster.com>
2024-02-16 16:42:05 +0100

    feat: add pagiation to profile page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
 	import type { Domain } from '@metanames/sdk';
-
 	import { walletAddress, walletConnected } from '$lib/stores/main';
 	import { metaNamesSdk } from '$lib/stores/sdk';
 
 	import Paper from '@smui/paper';
-	import DataTable, { Head, Body, Row, Cell, Label, SortValue } from '@smui/data-table';
+	import DataTable, { Head, Body, Row, Cell, Label, SortValue, Pagination } from '@smui/data-table';
 	import IconButton from '@smui/icon-button';
 	import LinearProgress from '@smui/linear-progress';
 	import Button from '@smui/button/src/Button.svelte';
+	import Select, { Option } from '@smui/select';
 
 	let domains: Domain[] = [];
 	let sort: keyof Domain = 'tokenId';
 	let sortDirection: Lowercase<keyof typeof SortValue> = 'ascending';
+	let rowsPerPage = 5;
+	let currentPage = 0;
+
+	$: start = currentPage * rowsPerPage;
+	$: end = Math.min(start + rowsPerPage, domains.length);
+	$: slice = domains.slice(start, end);
+	$: lastPage = Math.max(Math.ceil(domains.length / rowsPerPage) - 1, 0);
+	$: if (currentPage > lastPage) {
+		currentPage = lastPage;
+	}
 
 	let loaded = false;
 
@@ -73,7 +83,7 @@
 					</Head>
 					{#if loaded && domains.length > 0}
 						<Body>
-							{#each domains as domain (domain.tokenId)}
+							{#each slice as domain (domain.tokenId)}
 								<Row>
 									<Cell numeric>{domain.tokenId}</Cell>
 									<Cell>
@@ -88,6 +98,48 @@
 							{/each}
 						</Body>
 					{/if}
+					<Pagination slot="paginate">
+						<svelte:fragment slot="rowsPerPage">
+							<Label>Rows Per Page</Label>
+							<Select variant="outlined" bind:value={rowsPerPage} noLabel>
+								<Option value={5}>5</Option>
+								<Option value={10}>10</Option>
+								<Option value={20}>20</Option>
+							</Select>
+						</svelte:fragment>
+						<svelte:fragment slot="total">
+							{start + 1}-{end} of {domains.length}
+						</svelte:fragment>
+
+						<IconButton
+							class="material-icons"
+							action="first-page"
+							title="First page"
+							on:click={() => (currentPage = 0)}
+							disabled={currentPage === 0}>first_page</IconButton
+						>
+						<IconButton
+							class="material-icons"
+							action="prev-page"
+							title="Prev page"
+							on:click={() => currentPage--}
+							disabled={currentPage === 0}>chevron_left</IconButton
+						>
+						<IconButton
+							class="material-icons"
+							action="next-page"
+							title="Next page"
+							on:click={() => currentPage++}
+							disabled={currentPage === lastPage}>chevron_right</IconButton
+						>
+						<IconButton
+							class="material-icons"
+							action="last-page"
+							title="Last page"
+							on:click={() => (currentPage = lastPage)}
+							disabled={currentPage === lastPage}>last_page</IconButton
+						>
+					</Pagination>
 					<LinearProgress
 						closed={loaded}
 						indeterminate
@@ -108,12 +160,19 @@
 
 <style lang="scss">
 	.profile {
-		min-width: 60vw;
+		width: 70vw;
+		margin: 2rem auto;
 	}
 
 	.paper-content {
 		padding: 1rem;
 		text-align: center;
+	}
+
+	@media screen and (max-width: 768px) {
+		.profile {
+			width: 90vw;
+		}
 	}
 
 	h3 {
