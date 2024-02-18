ce3a07cc6546c92188db213e6d70f3f303a71c57
Marco <marco@yeboster.com>
2024-02-18 17:34:12 +0100

    fix(config): load env variable

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -1,13 +1,10 @@
-import { config } from "../config"
+import { config } from "../config";
+import { PROPOSALS_WALLET_PRIVATE_KEY } from '$env/static/private';
 
-if (!process.env.PROPOSALS_WALLET_PRIVATE_KEY) {
-  throw new Error('PROPOSALS_WALLET_PRIVATE_KEY is not set')
-}
-
-export const proposalsWalletPrivateKey = `${process.env.PROPOSALS_WALLET_PRIVATE_KEY}`
+export const proposalsWalletPrivateKey = `${PROPOSALS_WALLET_PRIVATE_KEY}`;
 
 const tldMigrationProposal = {
   'mainnet': '02bdce8a432de1de5e8d0d413517c2e10ebb0d3e80',
   'testnet': '021e68773e9bd5fc28381802c4b24899499f039ea9'
-}
-export const tldMigrationProposalContractAddress = config.environment === 'prod' ? tldMigrationProposal.mainnet : tldMigrationProposal.testnet
+};
+export const tldMigrationProposalContractAddress = config.environment === 'prod' ? tldMigrationProposal.mainnet : tldMigrationProposal.testnet;
