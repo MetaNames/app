73b9ba9e6113ee9803755b83a5ec605e465f42d3
Marco <marco@yeboster.com>
2024-02-18 17:17:56 +0100

    feat(config): add tld migration proposal mainnet contract

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -1,8 +1,13 @@
+import { config } from "../config"
+
 if (!process.env.PROPOSALS_WALLET_PRIVATE_KEY) {
   throw new Error('PROPOSALS_WALLET_PRIVATE_KEY is not set')
 }
 
 export const proposalsWalletPrivateKey = `${process.env.PROPOSALS_WALLET_PRIVATE_KEY}`
 
-// TODO: Add resolver based on environment
-export const tldMigrationProposalContractAddress = '021e68773e9bd5fc28381802c4b24899499f039ea9'
+const tldMigrationProposal = {
+  'mainnet': '02bdce8a432de1de5e8d0d413517c2e10ebb0d3e80',
+  'testnet': '021e68773e9bd5fc28381802c4b24899499f039ea9'
+}
+export const tldMigrationProposalContractAddress = config.environment === 'prod' ? tldMigrationProposal.mainnet : tldMigrationProposal.testnet
