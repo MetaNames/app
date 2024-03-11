a8354e86f49061083d088da57c7ee6024f832883
Marco <marco@yeboster.com>
2024-03-11 16:48:08 +0100

    fix(config): remove sentry maps to fix buildind

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-    plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "metanames",
-            project: "app"
-        }
-    }), sveltekit(), nodePolyfills(), tsconfigPaths()],
+    plugins: [sveltekit(), nodePolyfills(), tsconfigPaths()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     }
