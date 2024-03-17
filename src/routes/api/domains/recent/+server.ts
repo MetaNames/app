c601c4ffb964b4bc78d8c041a3addaa2a3a336f7
Marco <marco@yeboster.com>
2024-03-17 15:33:24 +0100

    refactor(api): recent domains endpoint

--- src/routes/api/domains/recent/+server.ts
+++ src/routes/api/domains/recent/+server.ts
@@ -1,21 +1,17 @@
-import { handleError, keyValueStore, metaNamesSdk, type DomainProjection } from "$lib/server";
+import { getRecentDomains, handleError, keyValueStore, type DomainProjection } from "$lib/server";
 import { json } from '@sveltejs/kit';
 
-export async function GET() {
+export async function GET({ url }) {
 	return handleError(async () => {
+    const refresh = url.searchParams.get('refresh')
+
+		let recentDomains: DomainProjection[]
 		const kv = keyValueStore()
-		if (kv) {
-			const cachedDomains = await kv.get<DomainProjection[]>('domains/recent')
-			if (cachedDomains)
-				return json(cachedDomains, { headers: { 'Cache-Control': 'max-age=500, public' } });
-		}
+		if (kv && !refresh) recentDomains = await kv.get<DomainProjection[]>('domains/recent').then((domains) => domains || [])
+		else recentDomains = await getRecentDomains()
 
-		const recentDomains: DomainProjection[] = await metaNamesSdk.domainRepository.getAll()
-			.then((domains) => domains.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()).slice(0, 12)
-				.map((domain) => ({ name: domain.name, createdAt: domain.createdAt })));
+		if (kv && refresh) await kv.set('domains/recent', recentDomains)
 
-		if (kv) await kv.set('domains/recent', recentDomains)
-
-		return json(recentDomains, { headers: { 'Cache-Control': 'max-age=500, public' } });
+		return json(recentDomains, { headers: { 'Cache-Control': 'max-age=600, public' } });
 	})
 }
