07f112efed4d25a436acf9f07a89b828d08850e1
Marco <marco@yeboster.com>
2024-03-19 21:55:50 +0100

    Revert "chore(vite): do not resolve crypto or steam"
    This reverts commit 5e04d5e1a3d8ede5e8cefa9065026339dcc5907e.

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,8 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-
 export default defineConfig({
     plugins: [sveltekit(), nodePolyfills(), tsconfigPaths()],
-    build: {
-        rollupOptions: {
-            plugins: [
-                nodeResolve({
-                    resolveId(source) {
-                        if (source === 'crypto' || source === 'stream') return false;
-
-                        return null;
-                    }
-                }),
-            ]
-        }
-    },
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     }
