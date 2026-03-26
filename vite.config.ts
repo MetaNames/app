b7b4443cd4f328e781a257852efbdd8329497939
Marco <marco@bon.so>
2026-03-26 17:04:24 +0100

    chore: bump to vite8

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills-vite8';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'metanames',
-				project: 'app'
-			}
-		}),
-		sveltekit(),
-		nodePolyfills({
-			include: ['buffer', 'crypto', 'stream']
-		}),
-		tsconfigPaths()
-	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'metanames',
+        project: 'app'
+      }
+    }),
+    sveltekit(),
+    nodePolyfills({
+      include: ['buffer', 'crypto', 'stream']
+    })
+  ],
+  resolve: {
+    tsconfigPaths: true
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
