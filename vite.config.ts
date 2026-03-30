7991d1a5225b9b9212f44f9b665468f6f1997047
Marco <marco@bon.so>
2026-03-30 22:56:42 +0200

    fix: ledger deps resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { nodePolyfills } from 'vite-plugin-node-polyfills-vite8';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	plugins: [
@@ -17,7 +21,13 @@ export default defineConfig({
 		})
 	],
 	resolve: {
-		tsconfigPaths: true
+		tsconfigPaths: true,
+		alias: {
+			'@ledgerhq/hw-transport-webusb': path.resolve(
+				__dirname,
+				'node_modules/@ledgerhq/hw-transport-webusb/lib/TransportWebUSB.js'
+			)
+		}
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
