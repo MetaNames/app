b82b6b5f0a6f8f3dc2f0f3ea2562f93e4a2891a3
Marco <marco@bon.so>
2026-03-30 19:08:13 +0200

    fix: add back sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { nodePolyfills } from 'vite-plugin-node-polyfills-vite8';
 
 export default defineConfig({
 	plugins: [
-		// sentrySvelteKit({
-		// 	sourceMapsUploadOptions: {
-		// 		org: 'metanames',
-		// 		project: 'app'
-		// 	}
-		// }),
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'metanames',
+				project: 'app'
+			}
+		}),
 		sveltekit(),
 		nodePolyfills({
 			include: ['buffer', 'crypto', 'stream']
