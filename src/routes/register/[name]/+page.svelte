62257e174c5fc0bdc9318d23bb97812959aff423
Marco <marco@bon.so>
2026-08-20 23:46:50 +0000

    fix(routes): narrow the optional name route param

--- src/routes/register/[name]/+page.svelte
+++ src/routes/register/[name]/+page.svelte
@@ -19,7 +19,7 @@
 	const isParentPresent = writable<boolean>();
 	const analyzed = writable<IDomainAnalyzed>();
 
-	const nameParam = $page.params.name;
+	const nameParam = $page.params.name ?? '';
 
 	$: domainName = $analyzed?.name;
 	$: parentDomainName = $analyzed?.parentId;
